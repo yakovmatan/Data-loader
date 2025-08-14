e06784853aa97b93e531ee4dc445caa5db2a0d01
yakov matan <yakovmatan@gmail.com>
2025-08-14 16:32:07 +0300

    sql files

---
+++ scripts/create-data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE table_for_project (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
---
+++ scripts/insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO table_for_project (first_name,last_name) VALUES ('shneor', 'zirkus');
+INSERT INTO table_for_project (first_name,last_name) VALUES ('david', 'teichman');
+INSERT INTO table_for_project (first_name,last_name) VALUES ('yakov','matan');
